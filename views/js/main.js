85b40b1458c405eca80e99c4c5a9da27601c8179
greg-colin <greg-colin@users.noreply.github.com>
2015-01-30 12:31:42 -0500

    Add minified pizza.html, use a manually-resized jpg for one page. Recreate minified js. Minify /views/pizza.html to pizza_min.html. Add an inline script to asynchronously load CSS to pizza.html.

--- views/js/main.js
+++ views/js/main.js
@@ -451,11 +451,13 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  // Optimized to reduce number of uses of 'document.querySelectorAll(".randomPizzaContainer");'
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzaContainer = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < randomPizzaContainer.length; i++) {
+      var dx = determineDx(randomPizzaContainer[i], size);
+      var newwidth = randomPizzaContainer[i].offsetWidth + dx + 'px';
+      randomPizzaContainer[i].style.width = newwidth;
     }
   }
 
